1852539d3fb239e4ba88dae0ec48d24965092766
syaifulandy <syaifulandy@users.noreply.github.com>
2026-05-18 05:51:05 +0700

    Update autoenum.sh

--- autoenum.sh
+++ autoenum.sh
@@ -759,7 +759,7 @@ enum_service() {
         (
           cd "$web_dir" || exit
           print_cmd "$FFUF_SCRIPT path \"$url/FUZZ\""
-          timeout 2m "$FFUF_SCRIPT" path "$url/FUZZ" 2>&1 | tee "$web_dir/ffuf.log"
+          timeout 6m "$FFUF_SCRIPT" path "$url/FUZZ" 2>&1 | tee "$web_dir/ffuf.log"
         )
       else
         warn "FFUF wrapper not found/executable: $FFUF_SCRIPT"
